7feef3a090defb9184049a676ef38d35729eb6fc
Brian Drelling <brian.drelling@gmail.com>
2020-08-06 16:10:09 -0500

    More updates to CustomFont

--- Sources/Kipple/SwiftUI/ViewModifiers/CustomFont.swift
+++ Sources/Kipple/SwiftUI/ViewModifiers/CustomFont.swift
@@ -2,10 +2,10 @@
 
 import SwiftUI
 
-// reference: https://www.hackingwithswift.com/quick-start/swiftui/how-to-use-dynamic-type-with-a-custom-font
+// source: https://stackoverflow.com/questions/56726869/is-it-possible-to-use-dynamic-type-sizes-with-a-custom-font-in-swiftui
 
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
-public struct CustomFont: ViewModifier {
+public struct CustomRelativeFont: ViewModifier {
     @Environment(\.sizeCategory) var sizeCategory
 
     public var name: String
@@ -20,13 +20,60 @@ public struct CustomFont: ViewModifier {
     }
 }
 
+// source: https://www.hackingwithswift.com/quick-start/swiftui/how-to-use-dynamic-type-with-a-custom-font
+
+@available(iOS 13, macCatalyst 13, tvOS 13, watchOS 6, *)
+public struct CustomScaledFont: ViewModifier {
+    @Environment(\.sizeCategory) var sizeCategory
+
+    public var name: String
+    public var size: CGFloat
+    public var weight: Font.Weight = .regular
+
+    public func body(content: Content) -> some View {
+        let scaledSize = UIFontMetrics.default.scaledValue(for: self.size)
+
+        return content.font(
+            Font.custom(self.name, size: scaledSize)
+                .weight(self.weight)
+        )
+    }
+}
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public struct CustomStaticFont: ViewModifier {
+    public var name: String
+    public var size: CGFloat
+    public var weight: Font.Weight = .regular
+
+    public func body(content: Content) -> some View {
+        content.font(
+            Font.custom(self.name, size: self.size)
+                .weight(self.weight)
+        )
+    }
+}
+
+// MARK: Extensions
+
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 public extension View {
     func customFont(name: String,
                     style: UIFont.TextStyle,
                     weight: Font.Weight = .regular) -> some View
     {
-        self.modifier(CustomFont(name: name, style: style, weight: weight))
+        self.modifier(CustomRelativeFont(name: name, style: style, weight: weight))
+    }
+
+    func customFont(name: String, size: CGFloat, weight: Font.Weight = .regular) -> some View {
+        self.modifier(CustomStaticFont(name: name, size: size, weight: weight))
+    }
+}
+
+@available(iOS 13, macCatalyst 13, tvOS 13, watchOS 6, *)
+public extension View {
+    func scaledFont(name: String, size: CGFloat, weight: Font.Weight = .regular) -> some View {
+        self.modifier(CustomScaledFont(name: name, size: size, weight: weight))
     }
 }
 
@@ -54,6 +101,23 @@ public extension View {
                     style: UIFont.TextStyle,
                     weight: Font.Weight = .regular) -> some View
     {
-        self.modifier(CustomFont(name: appFont.rawValue, style: style, weight: weight))
+        self.customFont(name: appFont.rawValue, style: style, weight: weight)
+    }
+
+    func customFont(_ appFont: AppFont,
+                    size: CGFloat,
+                    weight: Font.Weight = .regular) -> some View
+    {
+        self.customFont(name: appFont.rawValue, size: size, weight: weight)
+    }
+}
+
+@available(iOS 13, macCatalyst 13, tvOS 13, watchOS 6, *)
+public extension View {
+    func scaledFont(_ appFont: AppFont,
+                    size: CGFloat,
+                    weight: Font.Weight = .regular) -> some View
+    {
+        self.scaledFont(name: appFont.rawValue, size: size, weight: weight)
     }
 }
