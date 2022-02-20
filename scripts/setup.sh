71844d0b12f86458e1f3fa1970b8360aa696101d
Brian Drelling <brian@drelling.com>
2022-02-20 15:03:22 -0600

    Updated dependencies

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
 # Verify the script has been executed
-echo "Running onboarding script..."
+echo "Running setup script..."
 
 # 1 - Install and Verify Build Tools
 
 # ensure that homebrew is installed
 # TODO: Add check for version of homebrew
-command -v brew >/dev/null 2>&1 || { echo >&2 "Homebrew is not installed."; exit 1; }
+command -v brew >/dev/null 2>&1 || {
+    echo >&2 "Homebrew is not installed."
+    exit 1
+}
 
 # ensure that mint is installed
 # TODO: Add check for version of mint
-command -v mint >/dev/null 2>&1 || { echo >&2 "Mint is not installed."; exit 1; }
+command -v mint >/dev/null 2>&1 || {
+    echo >&2 "Mint is not installed."
+    exit 1
+}
 
 # 2 - Install Git Hooks
 
-# if there are any scripts in the scripts/git-hooks folder, 
+# if there are any scripts in the scripts/git-hooks folder,
 # copy them into the .git/hooks folder which is not source controlled
 # This will replace any existing files
-cp -R -a scripts/git-hooks/. .git/hooks
+scripts/install_git_hooks.sh
 
 # Verify the script has completed
-echo "Onboarding complete."
+echo "Setup complete."
