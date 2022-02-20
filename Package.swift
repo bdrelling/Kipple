2256435b7db73131d364b79fd6811697eaba0502
Brian Drelling <brian@drelling.com>
2022-02-20 15:09:05 -0600

    Removed Firebase/Auth packages, disabled Diagnostics

--- Package.swift
+++ Package.swift
@@ -12,18 +12,14 @@ let package = Package(
     ],
     products: [
         .library(name: "Kipple", targets: ["KippleCore", "KippleUI"]),
-        .library(name: "KippleAuth", targets: ["KippleAuth"]),
         .library(name: "KippleCore", targets: ["KippleCore"]),
-        .library(name: "KippleDiagnostics", targets: ["KippleDiagnostics"]),
-        .library(name: "KippleFirebase", targets: ["KippleFirebase"]),
+//        .library(name: "KippleDiagnostics", targets: ["KippleDiagnostics"]),
         .library(name: "KippleTesting", targets: ["KippleTesting"]),
         .library(name: "KippleUI", targets: ["KippleUI"]),
     ],
     dependencies: [
-        .package(name: "BetterCodable", url: "https://github.com/marksands/BetterCodable", .upToNextMinor(from: "0.4.0")),
-        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "8.7.0")),
-        .package(name: "Pulse", url: "https://github.com/kean/Pulse", .upToNextMinor(from: "0.18.0")),
-        .package(name: "Sentry", url: "https://github.com/getsentry/sentry-cocoa", .upToNextMajor(from: "7.4.2")),
+//        .package(name: "Pulse", url: "https://github.com/kean/Pulse", .upToNextMinor(from: "0.18.0")),
+//        .package(name: "Sentry", url: "https://github.com/getsentry/sentry-cocoa", .upToNextMajor(from: "7.4.2")),
         .package(name: "UIDeviceComplete", url: "https://github.com/Nirma/UIDeviceComplete", .upToNextMajor(from: "2.7.6")),
     ],
     targets: [
@@ -34,37 +30,16 @@ let package = Package(
                 .product(name: "UIDeviceComplete", package: "UIDeviceComplete", condition: .when(platforms: [.iOS])),
             ]
         ),
-        .target(
-            name: "KippleAuth",
-            dependencies: [
-                .target(name: "KippleDiagnostics"),
-                .target(name: "KippleFirebase"),
-            ]
-        ),
-        .target(
-            name: "KippleDiagnostics",
-            dependencies: [
-                .product(name: "Pulse", package: "Pulse"),
-                .product(name: "Sentry", package: "Sentry"),
-            ]
-        ),
-        .target(
-            name: "KippleFirebase",
-            dependencies: [
-                .product(name: "BetterCodable", package: "BetterCodable"),
-                .product(name: "FirebaseAuth", package: "Firebase"),
-                .product(name: "FirebaseFirestore", package: "Firebase"),
-                .product(name: "FirebaseFirestoreSwift-Beta", package: "Firebase"),
-                .target(name: "KippleDiagnostics"),
-            ]
-        ),
+//        .target(
+//            name: "KippleDiagnostics",
+//            dependencies: [
+//                .product(name: "Pulse", package: "Pulse"),
+//                .product(name: "Sentry", package: "Sentry"),
+//            ]
+//        ),
         .target(
             name: "KippleTesting",
-            dependencies: [
-                .target(name: "KippleAuth"),
-                .target(name: "KippleFirebase"),
-                .product(name: "FirebaseFirestore", package: "Firebase"),
-            ]
+            dependencies: []
         ),
         .target(
             name: "KippleUI",
@@ -72,19 +47,5 @@ let package = Package(
                 .target(name: "KippleCore"),
             ]
         ),
-        // Test Targets
-        .testTarget(
-            name: "KippleAuthTests",
-            dependencies: [
-                .target(name: "KippleAuth"),
-                .target(name: "KippleTesting"),
-            ]
-        ),
-        .testTarget(
-            name: "KippleFirebaseTests",
-            dependencies: [
-                .target(name: "KippleFirebase"),
-            ]
-        )
     ]
 )
--- Sources/KippleFirebase/CombineFirebase/FirestoreError.swift
+++
@@ -1,6 +0,0 @@
-// Copyright © 2021 Brian Drelling. All rights reserved.
-
-// Sourced from https://github.com/rever-ai/CombineFirebase
-public enum FirestoreError: Error {
-    case nilResultError
-}
