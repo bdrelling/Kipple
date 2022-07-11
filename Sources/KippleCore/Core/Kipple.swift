82c04ac56984ff7f2085bb74d328d3fc6295c75c
Brian Drelling <brian@drelling.com>
2022-07-11 02:13:03 -0500

    Moved isRunningInXcodePreview to KippleCore

--- Sources/KippleCore/Core/Kipple.swift
+++ Sources/KippleCore/Core/Kipple.swift
@@ -2,6 +2,14 @@
 
 import Foundation
 
+// Additional class members are defined in Kipple extensions within their respective Kipple libraries.
 public enum Kipple {
-    // Additional class members are defined in Kipple extensions within their respective Kipple libraries.
+    /// Whether or not this code is running within Xcode SwiftUI Previews, which has limited functionality.
+    static let isRunningInXcodePreview: Bool = {
+        #if DEBUG
+            ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+        #else
+            false
+        #endif
+    }()
 }
