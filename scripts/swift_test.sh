b35d4f946a7ec8f62427de1dac5300ee9293d14f
Brian Drelling <brian@drelling.com>
2022-08-24 00:41:43 -0500

    Added GitHub Actions workflow

--- scripts/swift_test.sh
+++ scripts/swift_test.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-mkdir -p $DEPLOY_DIRECTORY
+# Until this repository has tests, ensure the library builds.
+swift build -c debug
 
-swift test -c debug --enable-code-coverage
-cp $(swift test --show-codecov-path) "${DEPLOY_DIRECTORY}/codecov.json"
+# # Set our output directory. If none is set, use the DEPLOY_DIRECTORY environment variable by default.
+# output_directory=${1-$DEPLOY_DIRECTORY}
+
+# # If output_directory is set, use it and gather code coverage,
+# # copying the output to the directory.
+# # Otherwise, just run the test.
+# if [ ! -z $output_directory ]; then
+#     mkdir -p $output_directory
+
+#     swift test -c debug --enable-code-coverage
+#     cp $(swift test --show-codecov-path) "${output_directory}/codecov.json"
+# else
+#     swift test -c debug
+# fi
