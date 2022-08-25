41cb70205739a27a54c07010e8776b930f4df9ac
Brian Drelling <brian@drelling.com>
2022-08-24 22:13:15 -0500

    Added Xcode workflows

--- scripts/swift_test.sh
+++ scripts/swift_test.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-# Set our output directory. If none is set, use the DEPLOY_DIRECTORY environment variable by default.
-output_directory=${1-$DEPLOY_DIRECTORY}
-
-# If output_directory is set, use it and gather code coverage,
-# copying the output to the directory.
-# Otherwise, just run the test.
-if [ ! -z $output_directory ]; then
-    mkdir -p $output_directory
-
-    swift test -c debug --enable-code-coverage
-    cp $(swift test --show-codecov-path) "${output_directory}/codecov.json"
-else
-    swift test -c debug
-fi
+# For now, the project has no tests, so just build.
+swift build -c debug --enable-test-discovery
